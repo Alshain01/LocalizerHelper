dbcd18a17e5d173c844c675f0f82d07bd3f1ceef
Alshain01 <alshain01@suddenlink.net>
2017-09-20 17:36:19 -0500

    Initial Commit

---
+++ LocalizerHelper/LocalizerHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace LocalizerHelper
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class LocalizerHelper : UnityEngine.MonoBehaviour
+    {
+        const string path = "GameData/LocalizerHelper/PluginData/";
+        public void Awake()
+        {
+            var file = new System.IO.FileInfo(path + "Language.cfg");
+            using (System.IO.StreamReader stream = file.OpenText())
+            {
+                KSP.Localization.Localizer.SwitchToLanguage(stream.ReadLine());
+            }
+        }
+    }
+}
